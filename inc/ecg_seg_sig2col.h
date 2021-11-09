d738d581bb021ea4a2e7a70c926c3d2dec5092c8
neil.yu <neil.yu@egsitec.com>
2021-11-09 08:51:10 +0800

    + Implement ecg_seg_sig2col     - print_sig2col_ctr_param     - sig2col_ctr_fp_constructor     - sig2col_mat_fp     - sig2col_ctr_desctructor

---
+++ inc/ecg_seg_sig2col.h
@@ -0,0 +1,42 @@
+#ifndef __ECG_SEG_SIG2COL_H__
+#define __ECG_SEG_SIG2COL_H__
+#include <stdint.h>
+#include <limit.h>
+#include "arm_typedef.h"
+#include "arm_util.h"
+#include "ecg_seg_matrix.h"
+#include "ecg_response_def.h"
+#include "ecg_seg_util.h"
+
+#ifdef EN_SIG2COL_DBG
+#define SIG2COL_FUNC_ENTRACNCE                  FUNC_ENTRANCE_LOG
+#define SIG2COL_FUNC_EXIT                       FUNC_EXIT_LOG
+#define SIG2COL_PRINTF(x...)                    ree_printf(LOG_DEBUG, x)
+#define SIG2COL_LOG                             LOG_DEBUG
+#else
+#define SIG2COL_FUNC_ENTRACNCE                  do {} while (0)
+#define SIG2COL_FUNC_EXIT                       do {} while (0)
+#define SIG2COL_PRINTF                          do {} while (0)
+#define SIG2COL_LOG                             LOG_VERBOSE
+#endif
+
+#define SIG2COL_INVALID_IND                     (INT_MIN)
+
+typedef struct sig2col_ctr {
+    uint32_t cur_out_l;
+    uint32_t cur_out_pack_l;
+    uint32_t cur_k_l;
+    uint32_t cur_ele_num;
+    uint32_t max_out_l;
+    uint32_t max_out_pack_l;
+    uint32_t max_k_l;
+    uint32_t max_ele_num;
+    void *col_buf;
+} sig2col_ctr_t;
+
+int32_t sig2col_ctr_fp_constructor(uint32_t max_out_l, uint32_t max_k_l, sig2col_ctr_t **pp_ctr);
+int32_t sig2col_mat_fp(sig2col_ctr_t **pp_ctr, mat_sig_t *p_mat);
+void sig2col_print_mat(void);
+void sig2col_ctr_destructor(sig2col_ctr_t *p_ctr);
+
+#endif
---
+++ src/ecg_seg_sig2col.c
@@ -0,0 +1,101 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "arm_typedef.h"
+#include "arm_util.h"
+#include "ecg_seg_matrix.h"
+#include "ecg_seg_sig2col.h"
+#include "ecg_response_def.h"
+#include "ecg_seg_util.h"
+
+static void print_sig2col_ctr_param(sig2col_ctr_t *p_ctr)
+{
+    SIG2COL_FUNC_ENTRANCE;
+    ree_check_null_exit(p_ctr, EXIT_PRINT_SIG2COL_CTR_PARAM, "%s occurs error due to p_ctr is NULL", __func__);
+    SIG2COL_PRINTF("%s p_ctr->cur_out_l %d", __func__, p_ctr->cur_out_l);
+    SIG2COL_PRINTF("%s p_ctr->cur_out_pack_l %d", __func__, p_ctr->cur_out_pack_l);
+    SIG2COL_PRINTF("%s p_ctr->cur_k_l %d", __func__, p_ctr->cur_k_l);
+    SIG2COL_PRINTF("%s p_ctr->cur_ele_num %d", __func__, p_ctr->cur_ele_num);
+    SIG2COL_PRINTF("%s p_ctr->max_out_l %d", __func__, p_ctr->max_out_l);
+    SIG2COL_PRINTF("%s p_ctr->max_out_pack_l %d", __func__, p_ctr->max_out_pack_l);
+    SIG2COL_PRINTF("%s p_ctr->max_k_l %d", __func__, p_ctr->max_k_l);
+    SIG2COL_PRINTF("%s p_ctr->max_ele_num %d", __func__, p_ctr->max_ele_num);
+EXIT_PRINT_SIG2COL_CTR_PARAM:
+    SIG2COL_FUNC_EXIT;
+}
+
+int32_t sig2col_ctr_fp_constructor(uint32_t max_out_l, uint32_t max_k_l, sig2col_ctr_t **pp_ctr)
+{
+    SIG2COL_FUNC_ENTRANCE;
+    int32_t retval = ECG_SEG_OK;
+    uint32_t ele_num = 0;
+    ree_check_null_exit_retval(pp_ctr, retval, ECG_SEG_INVLAID_PARAM, EXIT_SIG2COL_CTR_CONTRUCTOR,
+                              "%s occurs error due to pp_ctr is NULL", __func__);
+    ree_check_true_exit_retval((*pp_ctr != NULL), retval, ECG_SEG_INVLAID_PARAM, EXIT_SIG2COL_CTR_CONTRUCTOR,
+                               "%s occurs error due to *pp_ctr is NULL", __func__);
+    *pp_ctr = ree_malloc(sizeof(sig2col_ctr_t));
+    ree_set(*pp_ctr, 0, sizeof(sig2col_ctr_t));
+    *pp_ctr->max_out_l = max_out_l;
+    *pp_ctr->max_out_pack_l = (max_out_l / FP_PACK_SIZE + 1) * FP_PACK_SIZE;
+    *pp_ctr->max_k_l = max_k_l;
+    *pp_ctr->max_ele_num = (*pp_ctr->max_out_pack_l)*(*pp_ctr->max_k_l);
+    print_sig2col_ctr_param(*pp_ctr);
+    ele_num =  *pp_ctr->max_ele_num;
+    ree_log(SIG2COL_LOG, "%s prepares to allocate %d floating point buffer", ele_num);
+    *pp_ctr->col_buf = ree_malloc(ele_num*ELE_FP_SIZE);
+    ree_check_null_exit_retval(*pp_ctr->col_buf, retval, ECG_SEG_ALLOC_FAILED, EXIT_SIG2COL_CTR_CONTRUCTOR,
+                               "%s occurs error due to *pp_ctr->col_buf is NULL", __func__);
+    ree_set(*pp_ctr->col_buf, 0, ele_num*ELE_FP_SIZE);
+EXIT_SIG2COL_CTR_CONTRUCTOR:
+    SIG2COL_FUNC_EXIT;
+    return retval;
+}
+
+int32_t sig2col_mat_fp(sig2col_ctr_t *p_ctr, mat_sig_t *p_mat)
+{
+    SIG2COL_FUNC_ENTRANCE;
+    int32_t retval = ECG_SEG_OK;
+    uint32_t ele_num = 0, sig_ind_w_padding = 0, col_ind = 0;
+    ree_check_null_exit_retval(p_mat, retval, ECG_SEG_INVALID_PARAM, EXIT_SIG2COL_MAT_FP,
+                               "%s occurs error due to p_mat is NULL", __func__);
+    ree_check_null_exit_retval(p_ctr, retval, ECG_SEG_INVALID_PARAM, EXIT_SIG2COL_MAT_FP,
+                               "%s occurs error due to p_ctr is NULL", __func__);
+    ree_check_null_exit_retval(p_ctr->col_buf, retval, ECG_SEG_INVALID_PARAM, EXIT_SIG2COL_MAT_FP,
+                               "%s occurs error due to p_ctr->col_buf is NULL", __func__);
+    p_ctr->cur_k_l = p_mat->col_h;
+    p_ctr->cur_out_l = p_mat->col_w;
+    p_ctr->cur_out_pack_l = (p_mat->col_w / FP_PACK_SIZE + 1)*FP_PACK_SIZE;
+    p_ctr->cur_ele_num = p_ctr->cur_k_l * p_ctr->cur_out_pack_l;
+    print_sig2col_ctr_param(p_ctr);
+    ele_num = p_ctr->cur_ele_num;
+    ree_set(p_ctr->col_buf, 0, ele_num*ELE_FP_SIZE);
+    
+    for (uint32_t col_h_ind = 0; p_ctr->cur_k_l; col_h_ind++)
+    {
+        sig_ind_w_padding = col_h_ind;
+        for (uint32_t col_w_ind = 0; p_ctr->cur_out_pack_l; col_w_ind++)
+        {
+            if (col_w_ind < p_ctr->cur_out_l)
+            {
+                sig_ind_w_padding += col_w_ind*p_mat->stride;
+            }
+            col_ind++;
+            SIG2COL_PRINTF("%04d ", col_ind);
+        }
+        SIG2COL_PRINTF("\n");
+    }
+
+EXIT_SIG2COL_MAT_FP:
+    SIG2COL_FUNC_EXIT;
+    return retval;
+}
+
+void sig2col_ctr_desctructor(sig2col_ctr_t *p_ctr)
+{
+    SIG2COL_FUNC_ENTRANCE;
+    ree_check_null_exit(p_ctr, retval, ECG_SEG_INVLAID_PARAM, EXIT_SIG2COL_CTR_CONTRUCTOR,
+                        "%s occurs error due to p_ctr is NULL", __func__);
+    ree_free(p_ctr->col_buf);
+EXIT_SIG2COL_CTR_CONTRUCTOR:
+    SIG2COL_FUNC_EXIT;
+}
