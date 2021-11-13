c46b9b9af599370d00a965b3651261285a9aedb7
neil.yu <neil.yu@egsitec.com>
2021-11-13 09:52:20 +0800

    + Modify ctagscope

--- ecg_seg_tag_gen.sh
+++ ecg_seg_tag_gen.sh
@@ -1,4 +1,4 @@
-find . -name '*.c' -o -name '*.java' -o -name '*.h' -o -name '*.cpp' -o -name '*.cc' -o -name '.sh' -o -name '.mk' -o -name '*.yml' > /home/chenwei/Documents/SelfStudy/Code/Code_Of_Android_ECG_SEG/ctagscpoe/ecg_seg.files 
-cscope -Rbkq -i ~/Documents/SelfStudy/Code/Code_Of_Android_ECG_SEG/ctagscpoe/ecg_seg.files -f ~/Documents/SelfStudy/Code/Code_Of_Android_ECG_SEG/ctagscpoe/ecg_seg.cout
-ctags -R -f /home/chenwei/Documents/SelfStudy/Code/Code_Of_Android_ECG_SEG/ctagscpoe/ecg_seg.tag --tag-relative=yes
+find . -name '*.c' -o -name '*.java' -o -name '*.h' -o -name '*.cpp' -o -name '*.cc' -o -name '.sh' -o -name '.mk' -o -name '*.yml' > /home/chenwei/Documents/SelfStudy/Code/Code_Of_Android_ECG_SEG/ctagscope/ecg_seg.files 
+cscope -Rbkq -i ~/Documents/SelfStudy/Code/Code_Of_Android_ECG_SEG/ctagscope/ecg_seg.files -f ~/Documents/SelfStudy/Code/Code_Of_Android_ECG_SEG/ctagscope/ecg_seg.cout
+ctags -R -f /home/chenwei/Documents/SelfStudy/Code/Code_Of_Android_ECG_SEG/ctagscope/ecg_seg.tag --tag-relative=yes
 
